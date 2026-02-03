0056caa8ee5e0961e4c85b290ec2876d6af5a2a4
ichrakjaifra <ichrak.jaifra@gmail>
2026-02-03 09:37:59 +0100

    MSF-65 Mettre à jour app.config.ts avec HttpClient

--- frontend/src/app/app.config.ts
+++ frontend/src/app/app.config.ts
@@ -1,11 +1,17 @@
-import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/core';
+import { ApplicationConfig, importProvidersFrom } from '@angular/core';
 import { provideRouter } from '@angular/router';
-
+import { provideAnimations } from '@angular/platform-browser/animations';
+import { provideHttpClient, withInterceptors } from '@angular/common/http';
 import { routes } from './app.routes';
+import { apiInterceptor } from './core/interceptors/api.interceptor';
+import { errorInterceptor } from './core/interceptors/error.interceptor';
 
 export const appConfig: ApplicationConfig = {
   providers: [
-    provideBrowserGlobalErrorListeners(),
-    provideRouter(routes)
+    provideRouter(routes),
+    provideAnimations(),
+    provideHttpClient(
+      withInterceptors([apiInterceptor, errorInterceptor])
+    )
   ]
 };
